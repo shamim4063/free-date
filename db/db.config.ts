c9853ab414b432e29f06c2a6c5a77b9f86d369ee
md shamim hossain <shamim.vc@gmail.com>
2024-01-01 11:10:23 +0000

    Updated by Bd url

--- db/db.config.ts
+++ db/db.config.ts
@@ -1,24 +1,20 @@
 import { registerAs } from "@nestjs/config";
-import { config as dotenvConfig } from 'dotenv';
+import { config as dotenvConfig } from "dotenv";
 import { DataSource, DataSourceOptions } from "typeorm";
 
-dotenvConfig({ path: '.env' });
+dotenvConfig({ path: ".env" });
 
 const config = {
-    type: 'postgres',
-    host: `${process.env.DB_HOST}`,
-    port: `${process.env.DB_PORT}`,
-    username: `${process.env.DB_USER}`,
-    password: `${process.env.DB_PASSWORD}`,
-    database: `${process.env.DB_NAME}`,
-    entities: ['dist/**/entities/*.entity.js'],
-    migrations: ['dist/db/migrations/*.js'],
-    migrationsTableName: 'db_migrations',
-    logging: true,
-    autoLoadEntities: true,
-    synchronize: false,
-}
+  type: "postgres",
+  url: `${process.env.DB_URL}`,
+  entities: ["dist/**/entities/*.entity.js"],
+  migrations: ["dist/db/migrations/*.js"],
+  migrationsTableName: "db_migrations",
+  logging: true,
+  autoLoadEntities: true,
+  synchronize: false,
+};
 
-export default registerAs('typeorm', () => config)
+export default registerAs("typeorm", () => config);
 export const connectionSource = new DataSource(config as DataSourceOptions);
-export const connectionOptions = config as DataSourceOptions
+export const connectionOptions = config as DataSourceOptions;
